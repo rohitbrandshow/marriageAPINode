7ab560339cda902bc2d039ac4e393cbb5c6764ad
rohitbrandshow <rohit.kumar@brandshow.in>
2023-11-29 18:51:07 +0530

    updated files

--- functions/api.js
+++
@@ -1,7 +0,0 @@
-// functions/api.js
-exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello from your API!" }),
-  };
-};
--- functions/server.js
+++ functions/server.js
@@ -25,122 +25,122 @@ db.once('open', () => {
 });
 
 //** Log Generate API Start **//
-const userLogRouter = require('./routes/userLog');
+const userLogRouter = require('../routes/userLog');
 app.use('/api/user-log', userLogRouter);
 //** Log Generate API End **//
 
 
 // Your API endpoint for user registration (as provided earlier)
-const registrationRouter = require('./routes/registration');
+const registrationRouter = require('../routes/registration');
 app.use('/api/register', registrationRouter);
 /* Registration Send API End */
 
 //** Lifestyle Add API Start **/
-const lifeStyleUpdate = require('./routes/lifestyleRoutes');
+const lifeStyleUpdate = require('../routes/lifestyleRoutes');
 app.use('/api/user-lifestyle', lifeStyleUpdate);
 //** Lifestyle Add API End **/
 
 //** Religious BG Add API Start **/
-const religiousBackgroundRoutes = require('./routes/religiousBackgroundRoutes');
+const religiousBackgroundRoutes = require('../routes/religiousBackgroundRoutes');
 app.use('/api/user-religious-bg', religiousBackgroundRoutes);
 //** Religious BG Add API End **/
 
 
 // ** Family Info Add API Start ** //
-const familyRoutes = require('./routes/familyRoutes');
+const familyRoutes = require('../routes/familyRoutes');
 app.use('/api/user-family-info', familyRoutes);
 // ** Family Info Add API End ** //
 
 //** Career and Educational Info Add API Start **//
-const eduCareerRoute = require('./routes/educationCareerRoutes.js');
+const eduCareerRoute = require('../routes/educationCareerRoutes.js');
 app.use('/api/user-education-career-info', eduCareerRoute);
 //** Career and Educational Info Add API End **//
 
 
  //** Basic Info Add API Start **//
- const basicInfoRoutes = require('./routes/basicInfoRoutes');
+ const basicInfoRoutes = require('../routes/basicInfoRoutes');
  app.use('/api/user-basic-info', basicInfoRoutes);
  //** Basic Info Add API ENd **//
 
  //** Upload Image Start **//
- const uploadImage = require('./routes/uploadImage');
+ const uploadImage = require('../routes/uploadImage');
  app.use('/api/upload', uploadImage);
  //** Upload Image End **//
 
 
 //** API to add about yourself start **//
-const aboutYourselfRoutes = require('./routes/aboutYourselfRoutes');
+const aboutYourselfRoutes = require('../routes/aboutYourselfRoutes');
 app.use('/api/user-about-yourself', aboutYourselfRoutes);
 //** API to add about yourself end **//
 
 
 /****** Mobile OTP Send API Start *********/
-const SendMobOTP = require('./routes/sendMobOTPRoutes');
+const SendMobOTP = require('../routes/sendMobOTPRoutes');
 app.use('/api/send-mob-otp', SendMobOTP);
 /****** Mobile OTP Send API End *********/
 
 
 // ** Mobile OTP Verification Start ** //
-const verifyOTP = require('./routes/verifyOTPRoutes');
+const verifyOTP = require('../routes/verifyOTPRoutes');
 app.use('/api/verify-mob-otp', verifyOTP);
 // ** Mobile OTP Verification End ** //
 
 /****** Email Send API Start *********/
-const emailOTP = require('./routes/sendEmailOTPRoutes');
+const emailOTP = require('../routes/sendEmailOTPRoutes');
 app.use('/api/send-email-otp', emailOTP);
 /****** Email Send API End *********/
 
 
 // ** Email OTP Verification Start ** //
-const verifyMailOTP = require('./routes/verifyMailOTPRoutes');
+const verifyMailOTP = require('../routes/verifyMailOTPRoutes');
 app.use('/api/verify-mail-otp', verifyMailOTP);
 // ** Email OTP Verification End ** //
 
-const loginRouter = require('./routes/loginAuthRoutes');
+const loginRouter = require('../routes/loginAuthRoutes');
 app.use('/api/login/returnLogin', loginRouter);
 
 //** Login Via Mobile API Start **//
-const logViaMob = require('./routes/loginViaMobRoutes');
+const logViaMob = require('../routes/loginViaMobRoutes');
 app.use('/api/login-via-mob', logViaMob);
 /* Login API ENd */
 
 /* Login API Start */
-const loginRoutes = require('./routes/loginAuthRoutes');
+const loginRoutes = require('../routes/loginAuthRoutes');
 app.use('/api/login', loginRoutes);
 /* Login API ENd */
 
 // ** Update User Status to Active and Return Session Start ** //
-const statusUpdateRoutes = require('./routes/statusUpdateRoutes');
+const statusUpdateRoutes = require('../routes/statusUpdateRoutes');
 app.use('/api/status-update', statusUpdateRoutes);
 // ** Update User Status to Active and Return Session End ** //
 
 
 // API to Add Basic Preferences Start
-const preferences = require('./routes/preferenceRoutes'); // Import the combined route for both basic and community preferences
+const preferences = require('../routes/preferenceRoutes'); // Import the combined route for both basic and community preferences
 app.use('/api/preferences', preferences); // Use a single route for both types
 // API to Add Basic Preferences ENd
 
 // API to Return User Data
-const userData = require('./routes/userDataRoutes'); // Import the combined route for both basic and community preferences
+const userData = require('../routes/userDataRoutes'); // Import the combined route for both basic and community preferences
 app.use('/api/user-data', userData); // Use a single route for both types
 // API to Return User Data ENd
 
-const recommendedData = require('./routes/recommendedProfile'); 
+const recommendedData = require('../routes/recommendedProfile'); 
 app.use('/api/recommendedData', recommendedData);
 
-const countData = require('./routes/profileCountRoutes'); 
+const countData = require('../routes/profileCountRoutes'); 
 app.use('/api/count', countData);
 
-const atlasSearchRoutes = require('./routes/atlasSearchRoutes');
+const atlasSearchRoutes = require('../routes/atlasSearchRoutes');
 app.use('/api/atlas-search', atlasSearchRoutes);
 
-const searchData = require('./routes/searchRoutes'); 
+const searchData = require('../routes/searchRoutes'); 
 app.use('/api/search', searchData);
 
-const adminFunction = require('./routes/adminRoutes'); 
+const adminFunction = require('../routes/adminRoutes'); 
 app.use('/api/admin', adminFunction);
 
-app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static('../uploads'));
 
 
 app.listen(port, () => {
